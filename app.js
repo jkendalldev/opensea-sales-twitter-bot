551b24e0cb2bbee48a4b8521384347eede60c9b8
Jason <jkendall1975@gmail.com>
2022-01-16 11:01:31 -0500

    v5

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const _ = require('lodash');
 const moment = require('moment');
 const { ethers } = require('ethers');
 // const tweet = require('./tweet');
-// const cache = require('./cache');
+const cache = require('./cache');
 
-console.log("LOOK V4 !!!!!!!!!!!!!!!!!!!!!!");
+console.log("LOOK v5 !!!!!!!!!!!!!!!!!!!!!!");
 
 var minutes = 1, the_interval = minutes * 60 * 1000;
 setInterval(function() {
