622966f5ff6f038f01d955de4a16b2b14a4fd0db
Jason <jkendall1975@gmail.com>
2022-01-16 10:46:47 -0500

    v2

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const axios = require('axios');
+const _ = require('lodash');
+const moment = require('moment');
+const { ethers } = require('ethers');
+const tweet = require('./tweet');
+const cache = require('./cache');
+
 var minutes = 1, the_interval = minutes * 60 * 1000;
 setInterval(function() {
 
-  console.log("LOOK...I am doing my X minutes check");
+  console.log("V2 .... LOOK...I am doing my X minutes check");
   // do your stuff here
 
 
