ed5a5006935ed1b6c8d91d73e24bbb94b8e49d60
Jason <jkendall1975@gmail.com>
2022-01-16 08:50:11 -0500

    update to 2 mins

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var minutes = 1, the_interval = minutes * 60 * 1000;
+var minutes = 2, the_interval = minutes * 60 * 1000;
 setInterval(function() {
   console.log("I am doing my 5 minutes check");
   // do your stuff here
