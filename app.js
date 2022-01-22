777e0a22c03249f97042b42d06d7fe78356b2a5b
Jason <jkendall1975@gmail.com>
2022-01-22 13:09:58 -0500

    Testing opensea testnet 6

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const cache = require('./cache');
 
 console.log("LOOK - START RUNNING!!!");
 
-axios.get('https://testnets-api.opensea.io/api/v1/collections?offset=0&limit=300').then(resp => {
-    console.log(resp.data);
+// https://testnets-api.opensea.io/api/v1/collections?offset=0&limit=300
+
+axios.get('https://testnets-api.opensea.io/api/v1/events', {
+  params: {
+    event_type: 'successful',
+    only_opensea: 'false',
+    collection_slug: 'clonex-mfz9ecb4mm',
+    asset_contract_address: '0x3ec9c3cb29ed95a396a48a4fbdb6b8546d001d5a',
+    token_id: '845'
+  }
+}).then(response => {
+    console.log(response.data);
 });
 
 /*
