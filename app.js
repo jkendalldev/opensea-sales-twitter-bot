ecf96348c4179640e36ea2f9787f70bb3b301602
Jason <jkendall1975@gmail.com>
2022-01-23 08:19:12 -0500

    Testing opensea testnet 29

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ const client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   });
 */
-const twitConfig({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-});
+
+const twitConfig = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+};
 
 console.log(twitConfig);
   
