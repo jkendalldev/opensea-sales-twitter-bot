ce5b61e142c4743fe2fb2fdee66af67111d97121
Jason <jkendall1975@gmail.com>
2022-01-22 19:29:25 -0500

    Testing opensea testnet 26

--- app.js
+++ app.js
@@ -5,18 +5,43 @@ const { ethers } = require('ethers');
 // const tweet = require('./tweet');
 const cache = require('./cache');
 
+/*
+// Format tweet text
+function formatAndSendTweet(event) {
+    // Handle both individual items + bundle sales
+    const assetName = _.get(event, ['asset', 'name'], _.get(event, ['asset_bundle', 'name']));
+    const openseaLink = _.get(event, ['asset', 'permalink'], _.get(event, ['asset_bundle', 'permalink']));
+
+    const totalPrice = _.get(event, 'total_price');
+
+    const tokenDecimals = _.get(event, ['payment_token', 'decimals']);
+    const tokenUsdPrice = _.get(event, ['payment_token', 'usd_price']);
+    const tokenEthPrice = _.get(event, ['payment_token', 'eth_price']);
+
+    const formattedUnits = ethers.utils.formatUnits(totalPrice, tokenDecimals);
+    const formattedEthPrice = formattedUnits * tokenEthPrice;
+    const formattedUsdPrice = formattedUnits * tokenUsdPrice;
+
+    const tweetText = `${assetName} bought for ${formattedEthPrice}${ethers.constants.EtherSymbol} ($${Number(formattedUsdPrice).toFixed(2)}) #NFT ${openseaLink}`;
+
+    console.log(tweetText);
+
+    // OPTIONAL PREFERENCE - don't tweet out sales below X ETH (default is 1 ETH - change to what you prefer)
+    // if (Number(formattedEthPrice) < 1) {
+    //     console.log(`${assetName} sold below tweet price (${formattedEthPrice} ETH).`);
+    //     return;
+    // }
+
+    // OPTIONAL PREFERENCE - if you want the tweet to include an attached image instead of just text
+    // const imageUrl = _.get(event, ['asset', 'image_url']);
+    // return tweet.tweetWithImage(tweetText, imageUrl);
+
+    return tweet.tweet(tweetText);
+}
+*/
+
 // Poll OpenSea every 60 seconds & retrieve all sales for a given collection in either the time since the last sale OR in the last minute
 setInterval(() => {
-
-console.log('\n');
-console.log('\n');
-console.log('\n');
-console.log("TOP OF setInterval LOOP...");
-console.log('\n');
-console.log('\n');
-console.log('\n');
-
-// so what happens with the API call when lastSaleTime is "undefined"?
 
 const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
 console.log(`******************** LAST SALE TIME: ${lastSaleTime}`);
@@ -58,12 +83,11 @@ params: {
         console.log("TWEEEEEEEEEEEEEEEEETING NOW !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         // return formatAndSendTweet(event);
     });
-// });
 
 }).catch((error) => {
     console.error(error);
 });
 }, 60000);
 
-// console.log("FINISH FINISH FINISH FINISH FINISH FINISH FINISH!");
+
     
