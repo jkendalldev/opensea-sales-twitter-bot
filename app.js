9cef06175a49dd54707937cbc3c06e09ad3fc1b4
Jason <jkendall1975@gmail.com>
2022-01-29 18:08:21 -0500

    adjust moment statement

--- app.js
+++ app.js
@@ -81,7 +81,8 @@ setInterval(() => {
 // original line below was set for 1mins:
 // const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(59, "seconds").unix();
 // set this line to ~ every 5mins
-const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(299, "seconds").unix();
+// we should be able to look back as far as we want in the moment().startOf side of the statement below...
+const lastSaleTime = cache.get('lastSaleTime', null) || moment().startOf('minute').subtract(12900, "seconds").unix();
 console.log(`******************** LAST SALE TIME: ${lastSaleTime}`);
 console.log(`Last sale (in seconds since Unix epoch): ${cache.get('lastSaleTime', null)}`);
 
